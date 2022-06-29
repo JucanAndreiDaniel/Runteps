5715014ac5629cd968a17ce85083f0e24efefe4e
Jucan Andrei Daniel <setrofex@gmail.com>
2022-06-29 22:08:37 +0300

    Add Class

--- frontend/src/pages/Class.jsx
+++ frontend/src/pages/Class.jsx
@@ -1,87 +1,82 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router";
 
-
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Container from '@mui/material/Container';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import LinearProgress from '@mui/material/LinearProgress';
+import LinearProgress from "@mui/material/LinearProgress";
 import GlobalStyles from "@mui/material/GlobalStyles";
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
-import Header from '../sections/Header';
-import { getClasses } from '../hooks/useClasses';
-import { styled } from '@mui/material/styles';
-
+import Header from "../sections/Header";
+import { getClass } from "../hooks/useClasses";
 
 function ClassCard(props) {
+  const { className, classId, description } = props;
 
-    const { className, classId, description } = props;
+  let progress = 10;
 
-    let progress = 10;
+  return (
+    <Card sx={{ minWidth: 275, maxWidth: 300 }}>
+      <CardContent>
+        <Stack spacing={2}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {className}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
 
-    return (
-        <Card sx={{ minWidth: 275, maxWidth: 300 }}>
-            <CardContent>
-                <Stack spacing={2}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {className}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {description}
-                    </Typography>
-
-                    <Box sx={{ width: '100%' }}>
-                        <Typography variant="body2" component="p">
-                            {progress}%
-                        </Typography>
-                        <LinearProgress variant="determinate" value={progress} />
-                    </Box>
-                </Stack>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Begin Course</Button>
-            </CardActions>
-        </Card>
-    );
+          <Box sx={{ width: "100%" }}>
+            <Typography variant="body2" component="p">
+              {classId}%
+            </Typography>
+            <LinearProgress variant="determinate" value={classId} />
+          </Box>
+        </Stack>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Begin Course</Button>
+      </CardActions>
+    </Card>
+  );
 }
 
-export default function Class(props) {
+export default function Class() {
+  const [classObj, setClassObj] = React.useState({});
 
-    const [classes, setClasses] = React.useState([]);
-    React.useEffect(() => {
-        getClasses().then(res => {
-            setClasses(res.data);
-        });
-    }, []);
+  let params = useParams();
+  React.useEffect(() => {
+    console.log(params.classId)
+    getClass(params.classId).then((res) => {
+      setClassObj(res.data);
+    });
+  }, []);
 
-    return (<>
-        <Header />
-        <GlobalStyles
-            styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+  return (
+    <>
+      <Header />
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
+      <CssBaseline />
+      <Container
+        disableGutters
+        maxWidth="xl"
+        component="main"
+        sx={{ pt: 8, pb: 6 }}
+      >
+        <ClassCard
+          className={classObj.name}
+          classId={classObj.id}
+          description={classObj.description}
         />
-        <CssBaseline />
-        <Container
-            disableGutters
-            maxWidth="xl"
-            component="main"
-            sx={{ pt: 8, pb: 6 }}
-        >
-            <Grid container spacing={3}>
-                {
-                    classes.map(classObj => {
-                        return <Grid item>
-                            <ClassCard className={classObj.name} classId={classObj.id} description={classObj.description} />
-                        </Grid>
-                    })
-                }
-            </Grid>
-        </Container>
-    </>);
+      </Container>
+    </>
+  );
 }
